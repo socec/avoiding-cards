229f9d3bb02919ebd03efb39f435629b00235a7a
Igor Socec <igorsocec@gmail.com>
2017-10-29 04:12:28 +0000

    client and server

---
+++ network/client.py
@@ -0,0 +1,64 @@
+import socket
+import threading
+
+
+class MessageHandler:
+    def handle(self, data: bytes):
+        pass
+
+
+class Client:
+    def __init__(self, server_address: str, server_port: int, message_handler: MessageHandler):
+        self._server_address = server_address
+        self._server_port = server_port
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._handler = message_handler
+        self._thread = threading.Thread(target=self._receive_messages)
+        self._running = False
+
+    def start(self):
+        self._socket.connect((self._server_address, self._server_port))
+        self._running = True
+        self._thread.start()
+
+    def stop(self):
+        self._running = False
+        self._thread.join()
+        self._socket.close()
+
+    def send_data(self, data: bytes):
+        try:
+            self._socket.sendall(data)
+        except:
+            print('Failed sending data to server')
+
+    def _receive_messages(self):
+        while self._running:
+            received_data = self._socket.recv(2048)
+            if len(received_data) > 0:
+                self._handler.handle(received_data)
+            else:
+                self._running = False
+                self._socket.close()
+                print('Server closed connection')
+
+    def is_running(self):
+        return self._running
+
+
+####################################################################################################
+
+class DefaultHandler(MessageHandler):
+    def handle(self, data: bytes):
+        print('server said: {}'.format(data))
+
+dh = DefaultHandler()
+
+client = Client('localhost', 4000, dh)
+client.start()
+while True:
+    import time
+    time.sleep(2)
+    client.send_data('KEEP\n'.encode())
+    if not client.is_running():
+        break
---
+++ network/server.py
@@ -0,0 +1,102 @@
+import socket
+import select
+import threading
+
+
+class Server:
+    def __init__(self, port: int, response_timeout: int, max_connections: int=7):
+        self._port = port
+        self._response_timeout = response_timeout  # seconds
+        self._max_connections = max_connections
+        self._clients = []
+        self._client_buffers = []
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._thread = threading.Thread(target=self._connection_handler)
+        self._lock = threading.Lock()
+        self._running = False
+
+    def start(self):
+        self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self._socket.bind(('', self._port))
+        self._socket.listen(self._max_connections)
+        self._running = True
+        self._thread.start()
+
+    def stop(self):
+        self._running = False
+        self._thread.join()
+        for client in self._clients:
+            client.close()
+        self._clients = []
+        self._client_buffers = []
+        self._socket.close()
+
+    def _connection_handler(self):
+        timeout = 0.05  # seconds
+        while self._running:
+            interesting_sockets = [self._socket] + self._clients
+            rlist, wlist, xlist = select.select(interesting_sockets, [], [], timeout)
+            with self._lock:
+                for incoming in rlist:
+                    if incoming == self._socket:
+                        # accept incoming connections
+                        client, address = incoming.accept()
+                        self._clients.append(client)
+                        self._client_buffers.append(bytearray())
+                    else:
+                        # check if client sockets are sending data or got disconnected
+                        received_data = incoming.recv(2048)
+                        if len(received_data) > 0:
+                            self._client_buffers[self._clients.index(incoming)] = received_data
+                        else:
+                            del self._client_buffers[self._clients.index(incoming)]
+                            self._clients.remove(incoming)
+                            print('Client disconnected: {}'.format(incoming.getsockname()))
+
+    def broadcast_data(self, data: bytes):
+        for client in self._clients:
+            try:
+                client.sendall(data)
+            except:
+                print('Failed broadcast to: {}'.format(client.getsockname()))
+
+    def send_to_client(self, data: bytes, client_id: int):
+        if len(self._clients) > client_id:
+            client = self._clients[client_id]
+            try:
+                client.sendall(data)
+            except:
+                print('Failed sending to client: {} @ {}'.format(client_id, client.getsockname()))
+        else:
+            print('Client does not exist: {}'.format(client_id))
+
+    def read_client_data(self, client_id: int) -> bytes:
+        data = bytearray()
+        if len(self._clients) > client_id:
+            with self._lock:
+                data = self._client_buffers[client_id]
+                self._client_buffers[client_id] = bytearray()
+        else:
+            print('Client does not exist: {}'.format(client_id))
+        return data
+
+    def get_connected_clients(self):
+        return self._clients
+
+    def is_running(self):
+        return self._running
+
+####################################################################################################
+
+server = Server(4000, 2)
+server.start()
+while True:
+    import time
+    time.sleep(2)
+    server.send_to_client('kaj ima\n'.encode(), 0)
+    server.broadcast_data('lima\n'.encode())
+    response = server.read_client_data(0)
+    if response:
+        print(response)
+    else:
+        print('jbg')
